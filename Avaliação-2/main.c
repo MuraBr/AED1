270c8ab2968216eec0937aadafea605c84f6591b
Erwin5642 <135025081+Erwin5642@users.noreply.github.com>
2023-05-31 11:41:06 -0400

    Update main.c

--- Avaliação-2/main.c
+++ Avaliação-2/main.c
@@ -1,1 +1,20 @@
 //União de todas as funções
+#include <stdio.h>
+
+int main ()
+{
+  //Declaração de variaveis
+  
+  
+  //Menu
+  
+    //Validação do CPF
+  
+      //Cédulas Fornecidas no Saque
+    
+    //Funções do Gerente
+  
+  //Saidas
+  
+  return 0;
+}
