be4f15f06d8204eb07b573ed913a06f9d0fe2990
Erwin5642 <135025081+Erwin5642@users.noreply.github.com>
2023-08-23 10:31:54 -0400

    Create cliente.c

---
+++ T3/cliente.c
@@ -0,0 +1,61 @@
+//Variáveis
+char cpfs[50][15], contasCorrentes[50][10];
+int saques[50][20];
+
+//Objetivo: gera um cpf e uma conta correte
+//verifica se não existe nenhum cpf ou conta corrente iguais já cadastrados
+//inclui o cliente no sistema
+//Parametros: vetor onde será armazenado o cpf, a conta corrente
+//Retorno: nenhum
+void incluirCliente(char cpfClientes[], char correnteClientes[]){
+
+}
+
+//Objetivo: verifica se não existe nenhum cpf ou conta corrente iguais já cadastrados
+//Parametros: vetores onde os cpfs e as contas estão armazenados
+//Retorno: 1 se existe e 0 em caso contrário
+int existeCadastro(char cpfClientes[], char correnteClientes[]){
+
+}
+
+//Objetivo: exibe os dados de cada cliente
+//Parametros: vetores onde os cpfs e as contas estão armazenados
+//Retorno: nenhum
+void mostraDadosClientes(char cpfClientes[], char correnteClientes[]){
+
+}
+
+//Objetivo: se não existir saque na conta do cliente, permite alterar o cpf ou a conta corrente, preservando a formatação
+//Parametros: vetores onde os cpfs e as contas estão armazenados
+//Retorno:nenhum
+void alterarDadosCliente(char cpfClientes[], char correnteClientes[]){
+
+}
+
+//Objetivo: altera o cpf de um cliente, mantendo formatação
+//Parametros: vetor onde o cpf está armazenado e deve ser modificado
+//Retorno:nenhum
+void alterarCpf(char cpfClientes[]){
+
+}
+
+//Objetivo: altera a conta corrente do cliente, mantendo formatação
+//Parametros: vetor onde a conta está armazenada e deve ser modificada
+//Retorno: nenhum
+void alterarContaCorrente(char correnteClientes[]){
+
+}
+
+//Objetivo:verifica se existe saques na conta do cliente
+//Parametros: vetor onde os saques dos clientes estão armazenados
+//Retorno: 1 se existe 0 em caso contrário
+int existeSaque(int saquesCliente[]){
+
+}
+
+//Objetivo: exclui cliente se não existe saques em sua conta
+//Parametros: vetor conta e cpf
+//Retorno: nenhum
+void excluirCliente(char cpfClientes[], char correnteClientes[]){
+
+}
