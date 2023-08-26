ebce89ea4a5275335072446ced86be021e75ef85
Erwin5642 <135025081+Erwin5642@users.noreply.github.com>
2023-08-26 00:33:54 -0400

    Update gerador.c

--- T3/gerador.c
+++ T3/gerador.c
@@ -25,100 +25,162 @@ char geraNumero() {
 // retorno:nenhum
 void geraContaCorrente(char c[])
 {
-    // implemente aqui
+// implemente aqui
     int i;
-    memset(c, 0, strlen(c));
+    char conta_processo[10];
+    memset(conta_processo, 0, strlen(conta_processo));
     for (i = 0; i < 8; i++)
     {
         if (i == 3)
         {
-            c[i] = '.';
+            conta_processo[i] = '.';
         }
         else if (i == 7)
         {
-            c[i] = '-';
+            conta_processo[i] = '-';
         }
         else
         {
-            c[i] = geraNumero();
+            conta_processo[i] = geraNumero();
         }
     }
-    c[8] = geraAlfabeto();
+    conta_processo[8] = geraAlfabeto();
+    strcpy(c, conta_processo);
 }
 
-//objetivo:verifica se um cpf no formato 999.999.999-99 e valido
-//parametros: cpf a ser verificado
-//retorno: 1 se cpf e valido ou 0 se cpf nao e valido
-int verifica_cpf_valido(char cpf[]) {
-   int valido=1;
-
-   //implemente aqui
-
-   return(valido);
+// objetivo:insere pontuacoes '.' e '- ' em um cpf
+// parametros: cpf_origem o cpf recebido no format 99999999999
+//             cpf_destino o cpf com as pontuacoes inseridas no formato 999.999.999-99
+void insere_pontuacao_cpf(char cpf_origem[], char cpf_destino[])
+{
+    // implemente aqui
+    char teste;
+    int cont = 0, i;
+    for (i = 0; i < 14; i++)
+    {
+        if (i == 3 || i == 7)
+        {
+            cpf_destino[i] = '.';
+        }
+        else if (i == 11)
+        {
+            cpf_destino[i] = '-';
+        }
+        else
+        {
+            teste = cpf_origem[cont];
+            cpf_destino[i] = teste;
+            cont++;
+        }
+    }
 }
-//objetivo:insere pontuacoes '.' e '- ' em um cpf
-//parametros: cpf_origem o cpf recebido no format 99999999999
-//            cpf_destino o cpf com as pontuacoes inseridas no formato 999.999.999-99
-void insere_pontuacao_cpf(char cpf_origem[], char cpf_destino[]) {
-
-   //implemente aqui
-   char teste;
-   int cont = 0, i;
-   for (i = 0; i < 14; i++)
-   {  
-      if (i==3 || i==7)
-      {
-         cpf_destino[i]='.';
-      }
-      else if (i==11)
-      {
-         cpf_destino[i]='-';
-      }
-      else
-      {
-        teste= cpf_origem[cont];
-         cpf_destino[i]=teste;
-         cont++;
-      }
-      
-   }
-
+// objetivo:calcula o primeiro digito verificador de um cpf no formato 999999999
+// parametros: cpf o cpf sem os digitos verificadores
+// retorno: o calculo do primeiro digito verificador
+int obtem_primeiro_digito_verificador(char cpf[])
+{
+    int digito = 0, intcpf, cont = 10, calccpf; // declaracao de variaveis
+    for (int i = 0; i < 9; i++)
+    { // converte digitos do cpf de char para int e faz o calculo necessario
+        intcpf = cpf[i] - '0';
+        digito += intcpf * cont;
+        cont -= 1;
+    }
+    if (digito * 10 % 11 == 10)
+    {
+        digito = 0;
+    }
+    else
+    {
+        digito = digito * 10 % 11;
+    }
+    return (digito);
 }
-//objetivo:calcula o primeiro digito verificador de um cpf no formato 999999999
-//parametros: cpf o cpf sem os digitos verificadores
-//retorno: o calculo do primeiro digito verificador
-int obtem_primeiro_digito_verificador(char cpf[]) {
-   int digito;
-
-   //implemente aqui
-
-   return(digito);
+// objetivo:calcula o segundo digito verificador de um cpf no formato 999999999
+// parametros: cpf sem os digitos verificadores
+// retorno: o calculo do segundo digito verificador
+int obtem_segundo_digito_verificador(char cpf[])
+{
+    int digito = 0, intcpf, cont = 11, calccpf, i; // declaracao de variaveis
+    for (int i = 0; i < 10; i++)
+    { // converte digitos do cpf de char para int e faz o calculo necessario
+        if (i <= 8)
+        {
+            intcpf = cpf[i] - '0';
+        }
+        else
+        {
+            intcpf = obtem_primeiro_digito_verificador(cpf);
+        }
+        digito += intcpf * cont;
+        cont -= 1;
+    }
+    if (digito * 10 % 11 == 10)
+    {
+        digito = 0;
+    }
+    else
+    {
+        digito = digito * 10 % 11;
+    }
+    return (digito);
 }
-//objetivo:calcula o segundo digito verificador de um cpf no formato 999999999
-//parametros: cpf sem os digitos verificadores
-//retorno: o calculo do segundo digito verificador
-int obtem_segundo_digito_verificador(char cpf[]) {
-   int digito;
 
-   //implemente aqui
+// objetivo:verifica se um cpf no formato 999.999.999-99 e valido
+// parametros: cpf a ser verificado
+// retorno: 1 se cpf e valido ou 0 se cpf nao e valido
+int verifica_cpf_valido(char cpf[])
+{
+    int d1cpf = cpf[12] - '0', d2cpf = cpf[13] - '0', valido = 1, count = 0, d1, d2, c = 0; // declaracao de variaveis
+    char cpf2[12];
 
-   return(digito);
+    for (int i = 0; i < 14; i++)
+    {
+        if ((cpf[i] == '.') || (cpf[i] == '-'))
+        {
+            ++i;
+        }
+        if (d1cpf == (cpf[i] + 0))
+        {
+            count++;
+        }
+        if (c <= 11)
+        {
+            cpf2[c] = cpf[i];
+        }
+        c++;
+    }
+    d1 = obtem_primeiro_digito_verificador(cpf2);
+    d2 = obtem_segundo_digito_verificador(cpf2);
+    if ((d1cpf == d1) && (d2cpf == d2) && (count < 11))
+    { // verificação do cpf
+        valido = 1;
+    }
+    else
+    {
+        valido = 0;
+    }
+    return (valido);
 }
-//objetivo:gera aleatoriamente um cpf valido no formato 999.999.999-99
-//parametros: cpf onde sera armazenado o cpf valido
-//retorno: nenhum
-void gera_cpf_valido(char cpf[]) {
-   //implemente aqui
+
+// objetivo:gera aleatoriamente um cpf valido no formato 999.999.999-99
+// parametros: cpf onde sera armazenado o cpf valido
+// retorno: nenhum
+void gera_cpf_valido(char cpf[])
+{
+    // implemente aqui
     int i;
     char cpf_processo[11];
-
-    do{
-        for(i = 0; i < 9; i++){
+    memset(cpf_processo, 0, strlen(cpf_processo));
+    do
+    {
+        for (i = 0; i < 9; i++)
+        {
             cpf_processo[i] = geraNumero();
         }
-        cpf_processo[9] = obtem_primeiro_digito_verificador() + '0';
-        cpf_processo[10] = obtem_segundo_digito_verificador() + '0';
+        cpf_processo[9] = obtem_primeiro_digito_verificador(cpf_processo) + '0';
+        cpf_processo[10] = obtem_segundo_digito_verificador(cpf_processo) + '0';
 
         insere_pontuacao_cpf(cpf_processo, cpf);
-    }while(verifica_cpf_valido(cpf) != 1);
+    } while (verifica_cpf_valido(cpf) != 1);
 }
