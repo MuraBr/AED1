36a6996355347d6ba301cfd9c471d04d16af409a
Erwin5642 <135025081+Erwin5642@users.noreply.github.com>
2023-08-26 20:01:05 -0400

    Update main.c

--- T3/main.c
+++ T3/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 // Variaveis para funções que envolvam cedulas
 const int tipoDaCedula[8] = {
     500, 150, 50, 20, 10, 5, 2, 1};
@@ -800,15 +801,15 @@ void alterarDadosCliente(char cpfClientes[50][15], char correnteClientes[50][10]
 // Retorno: nenhum
 void excluirCliente(char cpfClientes[50][15], char correnteClientes[50][10], int saquesClientes[50][15])
 {
-    //declaração de variaveis
+    // declaração de variaveis
     int numDoCliente, i, j, existe;
     char cpfClientesCopy[50][15], correnteClientesCopy[50][10], contaCliente[10];
     printf("Digite o numero de conta do cliente que deseja excluir:\n");
-    //escaneamento de
+    // escaneamento de
     scanf("%s", contaCliente);
     existe = existeContaCorrente(contaCliente, correnteClientes);
     numDoCliente = obtemNumeroDoCliente(contaCliente, correnteClientes);
-    if (existe==0)
+    if (existe == 0)
     {
         printf("Cliente inexistente!\n");
     }
@@ -820,25 +821,25 @@ void excluirCliente(char cpfClientes[50][15], char correnteClientes[50][10], int
         }
         else
         {
-            if(numDoCliente==numClientes-1)
+            if (numDoCliente == numClientes - 1)
             {
-                memset(correnteClientes[numDoCliente],0,10);
-                memset(saquesClientes[numDoCliente],0,15);
-                memset(cpfClientes[numDoCliente],0,15);
+                memset(correnteClientes[numDoCliente], 0, 10);
+                memset(saquesClientes[numDoCliente], 0, 15);
+                memset(cpfClientes[numDoCliente], 0, 15);
             }
             else
             {
-                i=0;
-                while((strcmp(correnteClientes[numDoCliente+1+i],"")!=0)&&(i<50-numClientes+numDoCliente+1))
+                i = 0;
+                while ((strcmp(correnteClientes[numDoCliente + 1 + i], "") != 0) && (i < 50 - numClientes + numDoCliente + 1))
                 {
-                    strcpy(correnteClientes[numDoCliente+i],correnteClientes[numDoCliente+1+i]);
-                    strcpy(cpfClientes[numDoCliente+i],cpfClientes[numDoCliente+1+i]);
-                    memcpy(saquesClientes[numDoCliente+i],saquesClientes[numDoCliente+1+i],15);
+                    strcpy(correnteClientes[numDoCliente + i], correnteClientes[numDoCliente + 1 + i]);
+                    strcpy(cpfClientes[numDoCliente + i], cpfClientes[numDoCliente + 1 + i]);
+                    memcpy(saquesClientes[numDoCliente + i], saquesClientes[numDoCliente + 1 + i], 15);
                     i++;
                 }
-                memset(correnteClientes[numDoCliente+i],0,10);
-                memset(saquesClientes[numDoCliente+i],0,15);
-                memset(cpfClientes[numDoCliente+i],0,15);
+                memset(correnteClientes[numDoCliente + i], 0, 10);
+                memset(saquesClientes[numDoCliente + i], 0, 15);
+                memset(cpfClientes[numDoCliente + i], 0, 15);
             }
             numClientes--;
         }
@@ -935,7 +936,7 @@ int main()
 
     // Loop para zerar a matriz com os saques
     memset(saques, 0, sizeof(saques));
-    
+
     // Inicio do caixa
     while (pg == 0)
     {
