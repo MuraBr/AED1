82269732d4be5e5b34aad3560f074b5fde12fa10
Erwin5642 <135025081+Erwin5642@users.noreply.github.com>
2023-08-31 23:48:06 -0400

    Update eliminaSequenciaFraca.c
    Correção de alguns problemas: 
    -Substituição de memmove para strcpy com uso de uma string auxiliar pra copiar o resto e eliminar tudo de uma vez com memset 
    -Correção do tamanho da eliminação 
    -Loop dentro do módulo
    -Teste para quando não existe resto

--- T4/eliminaSequenciaFraca.c
+++ T4/eliminaSequenciaFraca.c
@@ -1,29 +1,29 @@
 #include <stdio.h> 
 #include <string.h>
 
-void eliminaSequenciaFraca(char bacteria[], char virus[]){
-  int tamanhoVirus;
-  char *sequenciaFraca, *resto;
+// Objetivo: elimina o código genético de uma bactéria fraco a infecção do vírus
+// Parametros: vetor que armazena o código genético de uma bactéria e vetor que armazena o RNA do vírus
+// Retorno: nehnum
+void eliminaSequenciaFraca(char bacteriaSaudavel[], char virusInfectador[])
+{
+    int tamanhoVirus, tamanhoBacteria;
+    char *sequenciaFraca, *resto, copia[1000];
+    tamanhoVirus = strlen(virusInfectador);
 
-  tamanhoVirus = strlen(virus);
-  sequenciaFraca = strstr(bacteria, virus);
-  resto = sequenciaFraca + tamanhoVirus;
-  memmove(sequenciaFraca, resto, strlen(resto));
-  memset(resto + (strlen(resto) - tamanhoVirus), 0, strlen(resto));
-}
-
-
-int main(){
-    char bacteriaSaudavel[1000], virusInfectador[1000], comp[1000];
-    printf("Escreva o DNA da bacteria:\n");
-    scanf("%s", bacteriaSaudavel);
-    printf("Escreva o RNA do virus:\n");
-    scanf("%s", virusInfectador);
-    while(strstr(bacteriaSaudavel, virusInfectador) != NULL){
-        eliminaSequenciaFraca(bacteriaSaudavel, virusInfectador);
+    while (strstr(bacteriaSaudavel, virusInfectador) != NULL)
+    {
+        tamanhoBacteria = strlen(bacteriaSaudavel);
+        sequenciaFraca = strstr(bacteriaSaudavel, virusInfectador);
+        
+        if(strlen(sequenciaFraca) != 1){
+            resto = sequenciaFraca + tamanhoVirus;
+            strcpy(copia, resto);
+            memset(sequenciaFraca, 0, strlen(sequenciaFraca));
+            strcpy(sequenciaFraca, copia);
+            memset(copia, 0, strlen(copia));
+        }
+        else{
+            memset(sequenciaFraca, 0, 1);            
+        }
     }
-    printf("A sequencia resistente ao virus eh:\n%s\n", bacteriaSaudavel);
-    scanf("%s", comp);
-    printf("%d\n", strcmp(comp, bacteriaSaudavel));
-    return 0;
 }
