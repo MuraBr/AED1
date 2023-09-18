8ef40c52e0d24cc49e074cdff5814d2b4730662b
Frostbite16 <84871179+Frostbite16@users.noreply.github.com>
2023-09-18 10:34:11 -0400

    Update maiusculo.c

--- T5/maiusculo.c
+++ T5/maiusculo.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+char *maisculo(char texto[]){
+    int i = 0;
+    for(i;i<strlen(texto);i++){
+        if(islower(texto[i])){
+            texto[i]=toupper(texto[i]);
+        }
+    }
+    return(texto);
+}
