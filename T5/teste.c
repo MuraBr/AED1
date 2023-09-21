5dd5269f73010669097611515886fda7901d708c
Erwin Johannes Hattori VI <135025081+Erwin5642@users.noreply.github.com>
2023-09-20 22:10:32 -0400

    Update defineQuebrasDeLinha.c
    Inclui três funções:
    -uma para encontrar o inicio da ultima palavra em uma linha;
    -uma que atribui para cada ponteiro no vetor linhas a posição de onde uma nova linha deve ser começada, respeitando o limite de 80 caracteres;
    -a função para exibir o texto formatado.

--- T5/teste.c
+++ T5/teste.c
@@ -1,23 +1,59 @@
-#include <string.h>
-#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define COMP_LINHA 80
+#define COMP_LINHAS 80
 
-void matrizaTexto(char texto[], char *textoMatriz[]){
-    int i, j, k;
-    char copia[COMP_LINHA + 1];
+char *encontraIncioPalavra(char *comecoLinha, char *finalMaximoSuportado)
+{
+    char *quebra, *espaco;
+    espaco = strchr(comecoLinha, 32);
 
-     for(j = 0; strlen(texto + j) > 80; j += k){
-        strncpy(copia, (texto + j), 80);
-        *(strrchr(copia, 32) + 1) = '0';
-        
+    if ((espaco > finalMaximoSuportado) || (espaco == NULL))
+        return NULL;
+
+    while ((espaco < finalMaximoSuportado) && (espaco != NULL))
+    {
+        quebra = strchr(espaco, 32) + 1;
+        espaco = strchr(quebra, 32);
+    }
+    return quebra;
+}
+
+void definePosQuebraDeLinha(char texto[], char *endLinhas[])
+{
+    int i, k;
+    endLinhas[0] = texto;
+
+    for (k = 1; strlen(endLinhas[k - 1]) > COMP_LINHAS; k++)
+    {
+        if (*(endLinhas[k - 1] + COMP_LINHAS - 1) == 32)
+        {
+            endLinhas[k] = endLinhas[k - 1] + COMP_LINHAS;
+        }
+        else
+        {
+            endLinhas[k] = encontraIncioPalavra(endLinhas[k - 1], endLinhas[k - 1] + COMP_LINHAS);
+        }
+    }
+    endLinhas[k] = endLinhas[k - 1] + strlen(endLinhas[k - 1]);
+}
+
+void imprimeTextoFormatado(char texto[], char *endLinhas[])
+{
+    int i, tamLinha; 
+    for (i = 0; *(endLinhas[i]) != '\0'; ++i)
+    {
+        tamLinha = abs(endLinhas[i] - endLinhas[i + 1]);
+        printf("%.*s\n", tamLinha, endLinhas[i]);
     }
 }
 
-int main(){
-    char text[]="William Henry Gates III KBE GCIH (Seattle, 28 de outubro de 1955) mais conhecido como Bill Gates,\
+int main()
+{
+    char text[] = "William Henry Gates III KBE GCIH (Seattle, 28 de outubro de 1955) mais conhecido como Bill Gates,\
  e um magnata, empresario, diretor executivo, investidor, filantropo e autor americano, que ficou conhecido por \
  fundar junto com Paul Allen a Microsoft a maior e mais conhecida empresa de software do mundo em termos de valor \
  de mercado. Gates ocupa atualmente o cargo de presidente nao-executivo da Microsoft alem de ser classificado \
@@ -36,7 +72,7 @@ int main(){
  a Traf-o-Data, porem os clientes desistiram do negocio quando descobriram a idade dos donos. Enquanto \
  estudavam em Harvard, os jovens desenvolveram um interpretador da linguagem BASIC para um dos primeiros \
  computadores pessoais a serem lancado nos Estados Unidos - o Altair 8800. Apos um modesto sucesso na \
- comercializa��o deste produto, Gates e Allen fundaram a Microsoft, uma das primeiras empresas no mundo \
+ comercializacao deste produto, Gates e Allen fundaram a Microsoft, uma das primeiras empresas no mundo \
  focadas exclusivamente no mercado de programas para computadores pessoais ou PCs. Gates adquiriu ao \
  longo dos anos uma fama de visionario (apostou no mercado de software na epoca em que o hardware era \
  considerado muito mais valioso) e de negociador agressivo, chegando muitas vezes a ser acusado por \
@@ -48,6 +84,11 @@ int main(){
  o programa e vendeu-o por US$ 8 milhoes, mantendo a licenca do produto. Este viria a ser o MS-DOS. \
  Fonte: https://pt.wikipedia.org/wiki/Bill_Gates";
 
-    printf("\n\n%s",text);
+    char **linhas;
+    int i, cont = 0;
+    
+    definePosQuebraDeLinha(text, linhas);
+    imprimeTextoFormatado(text, linhas);
+
     return 0;
 }
