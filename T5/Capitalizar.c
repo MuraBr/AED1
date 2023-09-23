76035057a3f777f37add2851adac4f66225a92b3
FrostBite <84871179+Frostbite16@users.noreply.github.com>
2023-09-22 21:08:12 -0400

    Arrumei o capitalizar

--- T5/Capitalizar.c
+++ T5/Capitalizar.c
@@ -5,15 +5,16 @@
 #include <math.h>
 #include "UpperLowerCase.c"
 
+
 /*
     AAAAAAAAAAAAAAA. AAAAAAAAAAAAAAAAA. AAAAAAAAAAAAAAAAA.
             ^                ^                  ^
             |                |                  |
        PontLinhas[0]    PontLinhas[1]      PontLinhas[2]
-  TODO: String muda dentro da função, mas não muda fora(????) tem que concertar isso
+            
 */
 
-void DefinePontos(char texto[], char *PontLinhas[]){
+int DefinePontos(char texto[], char *PontLinhas[]){
     int i, k;
     PontLinhas[0] = texto;
 
@@ -28,18 +29,23 @@ void DefinePontos(char texto[], char *PontLinhas[]){
             PontLinhas[k] = strstr(PontLinhas[k-1],". ")+2;
         }
     }
+    return(k-1);
 }
 
 void CapitalizarTexto(char text[], char *PontLinhas[]){
     char *texto;
-    int i = 0;
-    texto = minusculo(text);
-    for(i = 0; *(PontLinhas[i])!='\0';i++){
+    int i = 0,k;
+    minusculo(text);
+    k=DefinePontos(text,PontLinhas);
+    for(i = 0; i<=k;i++){
         
         if((*(PontLinhas[i])<=122)&&(*(PontLinhas[i])>=95)){
-            *(PontLinhas[i])-=32;
+            *(PontLinhas[i])=*(PontLinhas[i])-32;
+            
         }
+        
     }
+
 }
 
 
