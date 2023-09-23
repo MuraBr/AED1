36163c7de114965f01ad1ac8f5d3ea5f66a04b8d
Erwin Johannes Hattori VI <135025081+Erwin5642@users.noreply.github.com>
2023-09-22 22:49:28 -0400

    Update and rename teste.c to testes.c

--- T5/testes.c
+++ T5/testes.c
@@ -16,7 +16,7 @@ char *encontraIncioPalavra(char *comecoLinha, char *finalMaximoSuportado)
 
     while ((espaco < finalMaximoSuportado) && (espaco != NULL))
     {
-        quebra = espaco + 1;
+        quebra = strchr(espaco, 32) + 1;
         espaco = strchr(quebra, 32);
     }
     return quebra;
@@ -51,30 +51,83 @@ void imprimeTextoFormatado(char texto[], char *endLinhas[])
     }
 }
 
+void removeEspacosExtras(char texto[]){
+    char *espacoExtra, copia[3000];
+    int cont;
+    
+    for(espacoExtra = strstr(texto, "  "); espacoExtra != NULL; espacoExtra = strstr(texto, "  "))
+    {
+        for (cont = 1; *(espacoExtra + cont) == 32; cont++);
+        strcpy(copia, espacoExtra + cont);
+        memset(espacoExtra + 1, 0, strlen(espacoExtra + 1));
+        strcpy(espacoExtra + 1, copia);
+    }
+    
+}
+
+void maisculo(char texto[]){
+    int i;
+    for (i = 0; texto[i] != '\0'; i++)
+    {
+        if(islower(texto[i])){
+            texto[i] = toupper(texto[i]);
+        }
+    }
+}
+void minusculo(char texto[]){
+    int i;
+    for (i = 0; texto[i] != '\0'; i++)
+    {
+        if(isupper(texto[i])){
+            texto[i] = tolower(texto[i]);
+        }
+    }
+}
+
+int validaPonto(char *p){
+    if(*(p - 2) == 32){
+        return 0;
+    }
+    return 1;
+}
+
+void capitalizarTexto(char texto[]){
+    char *ponto;
+    minusculo(texto);
+    texto[0] = toupper(texto[0]);
+    
+    for (ponto = strstr(texto, ". "); ponto != NULL; ponto = strstr(ponto + 1, ". "))
+    {
+        if(validaPonto(ponto)){
+            *(ponto + 2) = toupper(*(ponto + 2));
+        }
+    }
+}
+
 int main()
 {
     char text[] = "William Henry Gates III KBE GCIH (Seattle, 28 de outubro de 1955) mais conhecido como Bill Gates,\
  e um magnata, empresario, diretor executivo, investidor, filantropo e autor americano, que ficou conhecido por \
  fundar junto com Paul Allen a Microsoft a maior e mais conhecida empresa de software do mundo em termos de valor \
  de mercado. Gates ocupa atualmente o cargo de presidente nao-executivo da Microsoft alem de ser classificado \
- regularmente como a pessoa mais rica do mundo, posicao ocupada por ele de 1995 a 2007, 2009, e de 2014 a 2017. E \
+ regularmente como a pessoa              rica do mundo, posicao ocupada por ele de 1995 a 2007, 2009, e de 2014 a 2017. E \
  um dos pioneiros na revolucao do computador pessoal. Gates nasceu em uma familia de classe media de Seattle. Seu \
  pai, William H. Gates, era advogado de grandes empresas, e sua mae, Mary Maxwell Gates, foi professora da \
  Universidade de Washington e diretora de bancos. Bill Gates e as suas duas irmas, Kristanne e Libby, \
  frequentaram as melhores escolas particulares de sua cidade natal, e Bill tambem participou do \
- Movimento Escoteiro ainda quando jovem. Bill Gates, foi admitido na prestigiosa Universidade \
+ Movimento Escoteiro ainda quando jovem. Bill Gates,                   foi admitido na prestigiosa Universidade \
  Harvard, (conseguindo 1590 SATs dos 1600 possiveis) mas abandonou os cursos de Matematica e \
  Direito no terceiro ano para dedicar-se a Microsoft. Trabalhou na Taito com o desenvolvimento de \
  software basico para maquinas de jogos eletronicos (fliperamas) ate seus 16 anos. Tambem \
  trabalhou como pesquisador visitante na University of Massachusetts at Amherst, UMASS, Estados Unidos, quando \
  com 17 anos, desenvolveu junto com Paul Allen um software para leitura de fitas magneticas, com informacoes \
- de trafego de veiculos, em um chip Intel 8008. Com esse produto, Gates e Allen criaram uma empresa, \
- a Traf-o-Data, porem os clientes desistiram do negocio quando descobriram a idade dos donos. Enquanto \
+ de trafego de veiculos, em um chip Intel 8008.                      Com esse produto, Gates e Allen criaram uma empresa, \
+ a Traf-o-Data, porem os clientes desistiram  do negocio quando descobriram a idade dos donos. Enquanto \
  estudavam em Harvard, os jovens desenvolveram um interpretador da linguagem BASIC para um dos primeiros \
  computadores pessoais a serem lancado nos Estados Unidos - o Altair 8800. Apos um modesto sucesso na \
  comercializacao deste produto, Gates e Allen fundaram a Microsoft, uma das primeiras empresas no mundo \
  focadas exclusivamente no mercado de programas para computadores pessoais ou PCs. Gates adquiriu ao \
- longo dos anos uma fama de visionario (apostou no mercado de software na epoca em que o hardware era \
+ longo dos anos uma fama de visionario (apostou   no mercado de software na epoca em que o hardware era \
  considerado muito mais valioso) e de negociador agressivo, chegando muitas vezes a ser acusado por \
  concorrentes da Microsoft de utilizar praticas comerciais desleais. Nos anos 1980, a IBM, lider \
  no mercado de grandes computadores, resolveu entrar no mercado da microinformatica com o PC, \
@@ -83,12 +136,16 @@ int main()
  desenvolvido o sistema para o processador da Intel e decidiu compra-lo, pagou cerca de US$ 50 mil, personalizou \
  o programa e vendeu-o por US$ 8 milhoes, mantendo a licenca do produto. Este viria a ser o MS-DOS. \
  Fonte: https://pt.wikipedia.org/wiki/Bill_Gates";
+    int i;
+    char *linhas[50];
 
-    char **linhas;
-    int i, cont = 0;
-    
+    removeEspacosExtras(text);
+    maisculo(text);
     definePosQuebraDeLinha(text, linhas);
     imprimeTextoFormatado(text, linhas);
-
+    minusculo(text);
+    imprimeTextoFormatado(text, linhas);
+    capitalizarTexto(text);
+    imprimeTextoFormatado(text, linhas);
     return 0;
 }
