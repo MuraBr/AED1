bc69b0514d703fbd87deb7bc99f991793041bb7c
zeincola <38302337+zeincola@users.noreply.github.com>
2023-10-01 06:16:07 -0400

    Update alinhar_esquerda.c

--- T5/alinhar_esquerda.c
+++ T5/alinhar_esquerda.c
@@ -1,21 +1,29 @@
-//ainda vou comentar e corrigir alguns bugs
+// Objetivo: Alinhar o texto à esquerda
+// Parametros: vetor do texto
+// Retorno: nenhum
 void alinhar_esquerda(char *texto) {
     int contador = 80, contadorReserva = 0, tamanho = strlen(texto);
 
+    //loop de projeção das linhas, que se encerra ao concluir o texto
     while (contador < tamanho) {
-
+        
+        //loop que calcula o fim da linha, sem que haja quebra de uma palavra
         while (isspace(texto[contador]) == 0) {
             contador--;
         }
 
+        //loop que projeta a linha
         for (size_t i = contadorReserva; i < contador; i++) {
             printf("%c", texto[i]);
         }
 
+        //pulo de linha
         printf("\n");
 
+        //definição do contador reserva para próxima execução do loop "for"
         contadorReserva = contador + 1;
 
+        //definição do contador para próxima execução do loop "for"
         contador = contador + 80;
     }
 }
