d618aff9c8d233143d6029655114a12e72b33fa1
Erwin Johannes Hattori VI <135025081+Erwin5642@users.noreply.github.com>
2023-10-27 10:11:27 -0400

    Update main.c

--- T6/main.c
+++ T6/main.c
@@ -2,8 +2,28 @@
 #include <stdio.h>
 #include <math.h>
 #include "myList.h"
+/* Funcionalidades da biblioteca myList
+  //define uma lista como estrutura
+  struct lista {
+     int info;
+     struct lista* prox;
+  };
+  
+  typedef struct lista Lista;
+  
+  void inicializa (void);    //deve ser chamada antes de usar a estrutura
+  void insere (int);         //inserir um numero na estrutura fora da ordem
+  void insereOrdem (int);    //inserir um numero na estrutura na ordem
+  void imprime (void);       //imprimir o conteudo da estrutura
+  int busca (int);           //localiza um valor na estrutura, devolvendo 1 se encontrou ou 0 caso contrario
+  void retira (int);         //apaga um numero da estrutura
+  void esvazia();            //deve ser chamada quando nao for mais usar a estrutura
+*/
 
 int main(){
+  inicializa();
+  
+  esvazia();
   
   return 0;
 }
