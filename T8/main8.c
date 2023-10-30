d1d93d5d674ab2fac4e5f717a7293b8ab102851f
Erwin Johannes Hattori VI <135025081+Erwin5642@users.noreply.github.com>
2023-10-30 11:48:46 -0400

    Update main8.c

--- T8/main8.c
+++ T8/main8.c
@@ -32,3 +32,75 @@ no arquivo “carro.ord”.
 4. Para cada tipo de combustível informar a quantidade de carros.
 5. Para cada tipo de opcional informar a quantidade de carros.
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define TAM 50 
+typedef struct
+{
+    long int id_reg;
+    char placa[9];
+    char modelo[TAM];
+    char fabricante[TAM];
+    int ano_fabricacao;
+    int ano_modelo;
+    char combustivel[TAM];
+    char cor[TAM];
+    int opcional[8];
+    float preco_compra;
+}
+CARRO;
+
+char opcionais[][TAM] = {
+    {"4.portas"},
+    {"cambio.automatico"},
+    {"vidros.eletricos"},
+    {"abs"},
+    {"air.bag"},
+    {"ar.condicionado"},
+    {"banco.couro"},
+    {"sensor.estacionamento"}};
+
+/*
+1. Informar quantos registros tem “carro.dbf”.
+2. Ordenar os registros de “carro.dbf” de forma crescente pelo campo “placa” e armazená-los
+no arquivo “carro.ord”.
+3. Mostrar os registros de “carro.ord”.
+4. Para cada tipo de combustível informar a quantidade de carros.
+5. Para cada tipo de opcional informar a quantidade de carros.
+*/
+long int findSize(FILE *fp) 
+{ 
+    // checking if the file exist or not 
+    if (fp == NULL) { 
+        printf("File Not Found!\n"); 
+        return -1; 
+    } 
+  
+    fseek(fp, 0L, SEEK_END); 
+  
+    // calculating the size of the file 
+    long int res = ftell(fp); 
+  
+    // closing the file 
+  
+    return res; 
+}
+
+int main()
+{
+    char fileName[] = "/home/AD/rgm48935/ProjetoC/TrabalhosProvas/T8_AED1/carro.dbf";
+    FILE *dataBase = fopen(fileName, "rb");
+
+    if(dataBase == NULL){
+        printf("\nErro ao abrir o arquivo!\n");
+        return 0;
+    }
+    
+    printf("%lu\n", sizeof(dataBase));
+
+    return 0;
+}
