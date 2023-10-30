776281be8ce87cf129728d6dc82af81e5ea2c971
Erwin Johannes Hattori VI <135025081+Erwin5642@users.noreply.github.com>
2023-10-30 10:43:00 -0400

    Update main8.c

--- T8/main8.c
+++ T8/main8.c
@@ -1,7 +1,7 @@
-#define TAM 50
-char opcionais[][TAM] = {{"4.portas"}, {"cambio.automatico"}, {"vidros.eletricos"}, {"abs"}, {"air.bag"}, {"ar.condicionado"}, {"banco.couro"}, {"sensor.estacionamento"}};
-struct CARRO
+#define TAM
+50 typedef struct
 {
+    long int id_reg;
     char placa[9];
     char modelo[TAM];
     char fabricante[TAM];
@@ -11,84 +11,24 @@ struct CARRO
     char cor[TAM];
     int opcional[8];
     float preco_compra;
-};
-// AAA-1234
-// gol, celta, palio, ...
-// chevrolet, fiat, ...
-// 1980 à 2016
-// 1980 à 2017
-// alcool, gasolina, flex, diesel
-// branca, prata, preta
-// ver matriz opcionais
-struct ENDERECO
-{
-    char rua[TAM];
-    int numero;
-    char bairro[TAM];
-    char cidade[TAM];
-    char estado[3];
-    char cep[11];
-    // 99.999-999
-};
-struct TELEFONE
-{
-    char telefone[14]; // 99 99999-9999
-};
-struct CLIENTE
-{
-    char nome[TAM];
-    char cpf[15];
-    // 999.999.999-99
-    struct ENDERECO endereco;
-    struct TELEFONE residencial;
-    struct TELEFONE celular[5];
-    float renda_mensal;
-};
-struct DATA
-{
-    int dia, mes, ano;
-};
-struct VENDA_CARRO
-{
-    char placa_car[9];
-    char cpf_cli[15];
-    float preco_venda;
-    struct DATA data_venda;
-};
-/*
-Funções
----Carros---
-Inserir um carro no cadastro
-Excluir um carro do cadastro
-Listar os carros disponíveis para a venda ordenados crescentemente por fabricante e modelo
-Listar os carros disponíveis para a venda por seleção de um ou mais opcionais
-Listar os carros disponíveis para a venda por seleção da faixa de ano de fabricação
----Cliente---
-Inserir um cliente no cadastro
-Excluir um cliente do cadastro
-Listar os clientes do cadastro ordenados crescentemente pelo nome
-Listar os clientes do cadastro ordenados crescentemente pelo nome por seleção de faixa de renda salarial mensal
----Venda---
-Inserir uma venda
-Excluir uma venda
-Listar os carros vendidos de um determinado fabricante, ordenados crescentemente pelo modelo (as seguintes
-informações devem aparecer: modelo, placa, ano fabricação e nome cliente)
-Listar os carros vendidos de um determinado modelo, ordenados crescentemente pelo ano de fabricação (as
-seguintes informações devem aparecer: ano de fabricação, placa e nome cliente)
-Informar a quantidade de carros vendidos com o valor totalizado dos preços vendidos
-Informar o lucro total das vendas
-*/
+} CARRO;
+
+char opcionais[][TAM] = {
+    {"4.portas"},
+    {"cambio.automatico"},
+    {"vidros.eletricos"},
+    {"abs"},
+    {"air.bag"},
+    {"ar.condicionado"},
+    {"banco.couro"},
+    {"sensor.estacionamento"}};
+
 
 /*
-Observações:
-Para inserir um carro, gere automaticamente as informações do carro, e o usuário decide se o carro será inserido.
-Na exclusão de um carro o usuário fornece a placa do carro que deseja que seja excluído. Só poderá ser excluído um carro
-que ainda não foi vendido.
-Para inserir um cliente, gere automaticamente as informações do cliente, e o usuário decide se o cliente será inserido.
-Na exclusão de um cliente o usuário fornece o cpf do cliente que deseja que seja excluído. Só poderá ser excluído um
-cliente caso não há nenhuma venda registrada para ele.
-Para inserir uma venda tanto o carro como o cliente devem existir em seus respectivos cadastros.
-Para excluir uma venda o usuário fornece a placa do carro para pesquisa.
-O cpf do cliente deve ser válido.
-Exemplo de uma listagem de carros ordenados crescentemente por fabricante e modelo
+1. Informar quantos registros tem “carro.dbf”.
+2. Ordenar os registros de “carro.dbf” de forma crescente pelo campo “placa” e armazená-los
+no arquivo “carro.ord”.
+3. Mostrar os registros de “carro.ord”.
+4. Para cada tipo de combustível informar a quantidade de carros.
+5. Para cada tipo de opcional informar a quantidade de carros.
 */
