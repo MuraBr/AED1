d88401c6f4d7a475c762a5196fa0128d9785f015
Mathews Henrique Costa da Costa <86849333+MathewsH@users.noreply.github.com>
2023-11-01 09:47:25 -0400

    Update arthur.c

--- T6/arthur.c
+++ T6/arthur.c
@@ -13,32 +13,22 @@ int numAlgorismos(int num)
     return valor;
 }
 
-int numVetor(int vec[])
-{
-    int valor=0, i=0;
-    do{
-        valor++;
-        i++;
-    } while(vec[i]!='\0');
-    return valor;
-}
-
 int main()
 {   
     int a, b=0, n, k, pot, arr[10];
     long long int alg, len, num=0;
-    //Lista num;
-    //inicializa();
-    printf("Digita a: ");
-    scanf("%d", &a);
-    // printf("Digita n: ");
-    // scanf("%d", &n);
 
-    // for(int i=10; i<n; i++)
+    // printf("Digita a: ");
+    // scanf("%d", &a);
+    printf("Digita n: ");
+    scanf("%d", &n);
+
+    for(int i=10; i<=n; i++)
     {
+        num=0;
         k=0;
-        a=a;
-        b=a;
+        a=i;
+        b=i;
         len = numAlgorismos(a);
         pot = pow(10, len-1);
         for(int j=len; j>0; j--)
@@ -48,13 +38,10 @@ int main()
             pot/=10;
             k++;
         }
-        alg = numVetor(arr);
-        for(int i=0; i<=alg; i++)
+        for(int i=0; i<=len; i++)
         {
-            num+=pow(arr[i], alg);
+            num+=pow(arr[i], len);
         }
     }
-    for(int i=0; i<=10; i++) printf("%d ", arr[i]);
-    printf("\n%lld %lld\n", len, num);
     return 0;
 }
