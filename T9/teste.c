17701cfac05b18a6d770bbb2adbb05f8fd201195
FrostBite <84871179+Frostbite16@users.noreply.github.com>
2023-11-02 11:35:23 -0400

    Função Armazena pronta
    Adicionei um dir pro arquivo descriptografado na main
    Vou otimizar a função armazena depois - talvez

--- T9/teste.c
+++ T9/teste.c
@@ -1,34 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Armazena(FILE *arquivo){
+/*
+    Função que armazena codigo descriptografado em um arquivo .txt
+    Esse arquivo fica armazenado no diretório que o programa está sendo rodado
+*/
+void Armazena(FILE *arquivo, char* Dir){//arquivo: Arquivo criptografado, Dir: Nome do arquivo Descriptografado
     FILE *D_Palavras;
-    int i=0,l=0,c=0;
-    float n=0; 
-    D_Palavras = fopen("D_Palavras.txt", "w");
+    int letra=0,NumInt=0,c=1,co=0;
+
+    D_Palavras = fopen(Dir, "w");//Criando Arquivo onde serão armazenados os dados
+
+    /*
+        Este While loop vai pegar um carácter do Arquivo criptografado até chegar em um caracter ']'
+        então, a partir disso o programa vai ler todos os caracteres dentro dos '[]' ao contrario
+        multiplicando cada numero por multiplos de 10, Ex: [32] vai ser lido como 3*10 + 2*1.
+        Assim numeros é possivel inserir os numeros no arquivo descriptografado no formato char.
+    */
     do{
-        l=fgetc(arquivo);
-        if((l!='[')&&((l!=EOF))&&(l!=']')){
-            c*=10;
-            n+=l/c;
-            fprintf(D_Palavras,"%c",l);
+        letra = fgetc(arquivo);
+        if(letra==']'){
+            do{            
+                ++co;
+                fseek(arquivo,-2,SEEK_CUR);
+                if((letra != ']')&&(letra != 0)){
+                    NumInt+=(letra-'0')*c;
+                    c*=10;
+                }
+                letra=fgetc(arquivo);
+            }while(letra!='[');
+            fprintf(D_Palavras,"%c",NumInt);
+            fseek(arquivo,co,SEEK_CUR);
+            NumInt=0;
+            c=1;
+            co=0;
         }
-        else{
-            
-        }
-    }while(l!=EOF);
-    
+    }while(letra!=EOF);
 }
 
 int main()
 {
     FILE *Pfiletemp;
-    
-    char filename[]="/home/AD/rgm48931/Área de Trabalho/aed1/readme.code.txt";
-    if ((Pfiletemp = fopen(filename, "r"))==NULL){
-        printf("\nERRO abrindo %s\n",filename);
+    char *Dir="D_Palavras.txt";
+    if ((Pfiletemp = fopen("readme.code.txt", "r"))==NULL){
+        printf("\nERRO abrindo %s\n","readme.code.txt");
         exit(100);
     }
-    Armazena(Pfiletemp);
+    Armazena(Pfiletemp,Dir);
     
 }
