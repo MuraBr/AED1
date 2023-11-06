f1b883fec6c78a2e03bfe41d41222ee8efec3b71
Mathews.H <mathewscosta12@gmail.com>
2023-11-05 22:45:52 -0300

    Mathews

---
+++ T7/carro.c
@@ -0,0 +1,301 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 50
+
+int opcoesMenu(void)
+{
+    int opc;
+    printf("----------Menu------------\na)      Inserir um carro no cadastro;\n\
+    b)      Excluir um carro do cadastro;\n\
+    c)      Listar os carros disponíveis para a venda ordenados crescentemente por fabricante e modelo;\n\
+    d)      Listar os carros disponíveis para a venda por seleção de um ou mais opcionais;\n\
+    e)      Listar os carros disponíveis para a venda por seleção da faixa de ano de fabricação;\n\
+    f)      Encerrar programa\n\
+    Escolha uma opcao:\n");
+    scanf("%d", &opc);
+    return opc;
+}
+char opcionais[][TAM] = {{"4.portas"}, {"cambio.automatico"}, {"vidros.eletricos"}, {"abs"}, {"air.bag"}, {"ar.condicionado"}, {"banco.couro"}, {"sensor.estacionamento"}};
+typedef struct CARRO
+{
+    char placa[9];
+    char modelo[TAM];
+    char fabricante[TAM];
+    int ano_fabricacao;
+    int ano_modelo;
+    char combustivel[TAM];
+    char cor[TAM];
+    int opcional[8];
+    float preco_compra;
+}RCARRO;
+
+char geraAlfabeto() {
+    int i=0;
+    char letras[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+    i=rand()%26;
+    return(letras[i]);
+}
+
+char geraNumero() {
+    int i=0;
+    char numeros[] = { '0','1','2','3','4','5','6','7','8','9'};
+
+    i=rand()%9;
+    return(numeros[i]);
+}
+
+void GerarPlaca(char placa[]){
+
+    char PlacaCarro[9];
+    memset(PlacaCarro, 0, strlen(PlacaCarro));
+    for (int i = 0; i < 9; i++)
+    {
+        if (i<3){
+            PlacaCarro[i]=geraAlfabeto();
+        }
+        else if (i==3){
+            PlacaCarro[i]='-';
+        }
+        else{
+            PlacaCarro[i]=geraNumero();
+        }
+    }
+    strcpy(placa, PlacaCarro);
+    printf("Placa: %s\n",placa);
+}
+
+void gerarFabricante(char fabricante[]){
+    int i=0;
+    i=rand()%4;
+    if (i==0){
+       strcpy(fabricante, "fiat");
+    }
+    if (i==1){
+       strcpy(fabricante, "volkswagen");
+    }
+    if (i==2){
+       strcpy(fabricante, "toyota");
+    }
+    if (i==3){
+       strcpy(fabricante, "chevrolet");
+    }
+    printf("Fabricante: %s\n",fabricante);
+}
+void gerarModelo(char fabricante[],char modelo[], float preco_compra){
+    int j=0;
+    char fabricanteteste[20];
+    preco_compra=0;
+    strcpy(fabricanteteste, fabricante);
+    int teste =strcmp(fabricanteteste,"chevrolet");
+    if (teste==0){
+      j=rand()%3;
+      if (j==0){
+         strcpy(modelo, "Onix");
+         preco_compra=80000;
+      }
+      if (j==1){
+         strcpy(modelo, "S10");
+         preco_compra=100000;
+      }
+      if (j==2){
+         strcpy(modelo, "Cruze");
+         preco_compra=70000;
+      }
+    }
+
+    teste =strcmp(fabricanteteste,"toyota");
+    if (teste==0){
+      j=rand()%3;
+      if (j==0){
+         strcpy(modelo, "Corolla");
+         preco_compra=73000;
+      }
+      if (j==1){
+         strcpy(modelo, "Etios");
+         preco_compra=40000;
+      }
+      if (j==2){
+         strcpy(modelo, "Hilux");
+         preco_compra=120000;
+      }
+    }
+
+    teste =strcmp(fabricanteteste,"fiat");
+    if (teste==0){
+      j=rand()%3;
+      if (j==0){
+         strcpy(modelo, "Strada");
+         preco_compra=55000;
+      }
+      if (j==1){
+         strcpy(modelo, "Uno");
+         preco_compra=40000;
+      }
+      if (j==2){
+         strcpy(modelo, "Mobi");
+         preco_compra=50000;
+      }
+    }
+    teste =strcmp(fabricanteteste,"volkswagen");
+    if (teste==0){
+      j=rand()%3;
+      if (j==0){
+         strcpy(modelo, "Saveiro");
+         preco_compra=58000;
+      }
+      if (j==1){
+         strcpy(modelo, "Fox");
+         preco_compra=40000;
+      }
+      if (j==2){
+         strcpy(modelo, "Gol");
+         preco_compra=40000;
+      }
+    }
+    printf("Modelo: %s\n",modelo);
+}
+int gerarAnoFabricacao(int ano_fabricacao, float preco_compra){
+    int i=0;
+    i=rand()%35;
+    ano_fabricacao = 2000+(i-19);
+    int calculo=0;
+    calculo= ano_fabricacao-2016;
+    preco_compra = preco_compra+(1000*calculo);
+    printf("Ano de Fabricacao: %d\n",ano_fabricacao);
+    return ano_fabricacao;
+}
+void gerarAnoModelo(int ano_fabricacao, int ano_modelo){
+    int i=0;
+    i=rand()%3;
+    ano_modelo=0;
+    ano_modelo=ano_fabricacao+(i-1);
+    printf("Ano Modelo: %d\n",ano_modelo);
+    printf("Ano de Fabricacao: %d\n",ano_fabricacao);
+}
+
+void gerarGasolina(char combustivel[]){
+    int i=0;
+    i=rand()%4;
+    if (i==0){
+       strcpy(combustivel, "gasolina");
+    }
+    if (i==1){
+       strcpy(combustivel, "alcool");
+    }
+    if (i==2){
+       strcpy(combustivel, "flex");
+    }
+    if (i==3){
+       strcpy(combustivel, "diesel");
+    }
+    printf("Combustivel: %s\n",combustivel);
+}
+
+void gerarCor(char cor[]){
+    int i=0;
+    i=rand()%3;
+    if (i==0){
+       strcpy(cor, "branca");
+    }
+    if (i==1){
+       strcpy(cor, "prata");
+    }
+    if (i==2){
+       strcpy(cor, "preta");
+    }
+    printf("Cor: %s\n",cor);
+}
+void gerarOpcional(int opcional[]){
+    int k=0;
+    for (int j = 0; j < 8; j++)
+    {
+      k=rand()%2;
+      opcional[j]=k;
+    }
+    printf("Opcional: ");
+    for(int i=0; i<8;i++){
+      if (i==7)
+      {
+        if (opcional[i]==1)
+        {
+          printf("%s",opcionais[i]);
+        }
+      }
+      
+      if (opcional[i]==1)
+      {
+        printf("%s",opcionais[i]);
+        printf(", ",opcionais[i]);
+      }
+    }
+}
+
+void inserirCarro(RCARRO *c){
+    GerarPlaca(c->placa);
+    gerarFabricante(c->fabricante);
+    gerarAnoFabricacao(c->ano_fabricacao, c->preco_compra);
+    gerarModelo(c->fabricante,c->modelo, c->preco_compra);
+    gerarAnoModelo(c->ano_fabricacao,c->ano_modelo);
+    gerarGasolina(c->combustivel);
+    gerarCor(c->cor);
+    gerarOpcional(c->opcional);
+}
+
+void MostraCarro(){
+  char placa[9];
+  char modelo[TAM];
+  char fabricante[TAM];
+  int ano_fabricacao;
+  int ano_modelo;
+  char combustivel[TAM];
+  char cor[TAM];
+  int opcional[8];
+  float preco_compra =0;
+  GerarPlaca(placa);
+  gerarFabricante(fabricante);
+  gerarModelo(fabricante,modelo, preco_compra);
+  ano_fabricacao=gerarAnoFabricacao(ano_fabricacao, preco_compra);
+  gerarAnoModelo(ano_fabricacao,ano_modelo);
+  gerarGasolina(combustivel);
+  gerarCor(cor);
+  gerarOpcional(opcional);
+  printf("\nPreco de Compra: %f\n",preco_compra);
+}
+
+int main()
+{
+  int opc=0;
+  do
+  {
+    system("clear");
+    opc = opcoesMenu();
+    switch (opc)
+    {
+    case 1: //inserir carro
+      
+      printf("Deseja inserir o carro? Digite 1 para Sim e 2 para\n");
+      printf("Digite 1 para Sim e 0 para Não\n");
+      printf("------------------------------------------------------------------------------\n");
+      MostraCarro();
+        
+      break;
+    case 2: //
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+      printf("Encerrando programa...\n");
+      opc = 0;
+      break;
+    default:
+      printf("Opcao invalida!\n");
+      break;
+    }
+  } while (opc);
+  return 0;
+}
