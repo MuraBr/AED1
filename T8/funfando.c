e798a812d46a648e1ce737e8831a13f0498fecf6
Erwin Johannes Hattori VI <135025081+Erwin5642@users.noreply.github.com>
2023-11-08 11:45:47 -0400

    Create funfando.c
    yey

---
+++ T8/funfando.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM 50
+typedef struct
+{
+    long int id_reg;
+    char placa[9];
+    char modelo[TAM];
+    char fabricante[TAM];
+    int ano_fabricacao;
+    int ano_modelo;
+    char combustivel[TAM];
+    char cor[TAM];
+    int opcional[8];
+    float preco_compra;
+} CARRO;
+
+char opcionais[][TAM] = {
+    {"4.portas"},
+    {"cambio.automatico"},
+    {"vidros.eletricos"},
+    {"abs"},
+    {"air.bag"},
+    {"ar.condicionado"},
+    {"banco.couro"},
+    {"sensor.estacionamento"}};
+
+char tipoCombustivel[][TAM] = {
+    {"alcool"},
+    {"diesel"},
+    {"flex"},
+    {"gasolina"}};
+
+FILE *abrirArquivo(const char *nome, const char *modo)
+{
+    FILE *arquivo;
+    if ((arquivo = fopen(nome, modo)) == NULL)
+    {
+        printf("Nao foi possivel abrir o arquivo %s\n", nome);
+        exit(100);
+    }
+    printf("Arquivo %s aberto com sucesso\n", nome);
+    return arquivo;
+}
+
+void copiarArquivo(const char *destName, const char *srcName)
+{
+    CARRO c;
+    FILE *dest = abrirArquivo(destName, "wb");
+    FILE *src = abrirArquivo(srcName, "rb");
+
+    int cont = 0, tam = sizeof(CARRO);
+    while ((fread(&c, tam, 1, src)) && (cont < 32))
+    {
+        fwrite(&c, tam, 1, dest);
+        cont++;
+    }
+    fclose(dest);
+    fclose(src);
+}
+
+void mostrarArquivo(const char *fileName)
+{
+    CARRO buffer;
+    FILE *arquivo = abrirArquivo(fileName, "rb");
+    int tam = sizeof(CARRO);
+    while (fread(&buffer, tam, 1, arquivo))
+    {
+        printf("==========Carro==========\nID: %li\n", buffer.id_reg);
+        printf("Placa: %s\n", buffer.placa);
+        printf("Modelo: %s\n", buffer.modelo);
+        printf("Fabricante: %s\n", buffer.fabricante);
+        printf("Ano de Fabricacao: %d\n", buffer.ano_fabricacao);
+        printf("Ano do modelo: %d\n", buffer.ano_modelo);
+        printf("Tipo de combustivel: %s\n", buffer.combustivel);
+        printf("Cor: %s\n", buffer.cor);
+        printf("Preco: %.2f\n", buffer.preco_compra);
+    }
+    fclose(arquivo);
+}
+void mostrarRegistro(CARRO buffer)
+{
+    printf("==========Carro==========\nID: %li\n", buffer.id_reg);
+    printf("Placa: %s\n", buffer.placa);
+    printf("Modelo: %s\n", buffer.modelo);
+    printf("Fabricante: %s\n", buffer.fabricante);
+    printf("Ano de Fabricacao: %d\n", buffer.ano_fabricacao);
+    printf("Ano do modelo: %d\n", buffer.ano_modelo);
+    printf("Tipo de combustivel: %s\n", buffer.combustivel);
+    printf("Cor: %s\n", buffer.cor);
+    printf("Preco: %.2f\n", buffer.preco_compra);
+}
+
+void excluirRegistro(const char *fileName, CARRO excluir)
+{
+    FILE *arquivo = abrirArquivo(fileName, "rb");
+    CARRO aux;
+    int tam = sizeof(CARRO), flag = 0;
+
+    FILE *copia = abrirArquivo("copia.ord", "wb");
+
+    while (fread(&aux, tam, 1, arquivo))
+    {
+        if (memcmp(&aux, &excluir, tam) != 0)
+        {
+            fwrite(&aux, tam, 1, copia);
+        }
+        else
+        {
+            flag = 1;
+        }
+    }
+    if (flag == 0)
+    {
+        printf("O registro nao existe no arquivo %s\n", fileName);
+    }
+
+    fclose(arquivo);
+    fclose(copia);
+    remove(fileName);
+    rename("copia.ord", "carro.ord");
+}
+
+int bytesAteStruct(const char *fileName, CARRO busca)
+{
+    int bytes = 0, flag = 0, tam = sizeof(CARRO);
+    FILE *arquivo = abrirArquivo(fileName, "rb");
+    CARRO aux;
+
+    while (fread(&aux, tam, 1, arquivo))
+    {
+        if (memcmp(&aux, &busca, tam) != 0)
+        {
+            bytes += tam;
+        }
+        else
+        {
+            flag = 1;
+            break;
+        }
+    }
+    fclose(arquivo);
+    if (flag == 0)
+        bytes = -1;
+    return bytes;
+}
+// Retorna 0, se são iguais, >0 se b vem antes que a, e <0 se a vem antes de b
+int comparaPlacas(CARRO a, CARRO b)
+{
+    return memcmp(a.placa, b.placa, sizeof(a.placa));
+}
+
+void menorRegistroPlaca(const char *fileName, CARRO *endMenor)
+{
+    FILE *arquivo = abrirArquivo(fileName, "rb");
+    CARRO aux, menor;
+    int tam = sizeof(CARRO);
+    fread(&menor, tam, 1, arquivo);
+
+    while (fread(&aux, tam, 1, arquivo))
+    {
+        if (comparaPlacas(aux, menor) < 0)
+        {
+            memcpy(&menor, &aux, tam);
+        }
+    }
+    memcpy(endMenor, &menor, tam);
+    fclose(arquivo);
+}
+
+int main()
+{
+    const char original[] = "carro.dbf";
+    const char ordenado[] = "carro.ord";
+    CARRO aux, m;
+    FILE *testes;
+    int tam = sizeof(CARRO);
+    copiarArquivo(ordenado, original);
+    mostrarArquivo(ordenado);
+
+    testes = abrirArquivo(ordenado, "rb");
+    fread(&aux, tam, 1, testes);
+    fread(&aux, tam, 1, testes);
+    printf("Carro a ser excluido %s\n", aux.placa);
+    fclose(testes);
+    getchar();
+    menorRegistroPlaca("carro.ord", &m);
+    mostrarRegistro(m);
+    getchar();
+    excluirRegistro(ordenado, aux);
+    mostrarArquivo(ordenado);
+
+    return 0;
+}
